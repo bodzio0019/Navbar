05aa50e282f34fd5d7d547f0b544bea03803021e
Bodzio0019 <nikolas.ed@onet.eu>
2024-02-21 18:31:19 +0100

    Stworzono ikonkę rozwijania navbara oraz dodano animacje do wszystkich elementów

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const button = document.body.querySelector(".button");
 button.addEventListener("click", () => {
     document.body.querySelector("nav").classList.toggle("nav-js");
+    document.body.querySelector(".line-1").classList.toggle("line-1-js");
+    document.body.querySelector(".line-2").classList.toggle("line-2-js");
+    document.body.querySelector(".line-3").classList.toggle("line-3-js");
+    document.body.querySelector(".line-4").classList.toggle("line-4-js");
 });
